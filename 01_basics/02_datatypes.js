6e03d3bcbfda17f2b5e6ae24aeaa028b4e291fc3
Shriya Vanare <147965500+shriyavanare@users.noreply.github.com>
2023-10-15 07:19:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict";   // treat all JS code as newer version
+
+//alert("hello")  // we are using node js and not browser
+
+// code readability should be high
+console.log(3+3)
+
+console.log("shriya")
+
+let name = "shriya"
+let age = 23
+let isLoggedIn = true
+
+/* PRIMITIVE DATATYPES
+
+// number =>2 to power 53
+// big int
+// string => " "
+//boolean => true/false
+
+//null => stand alone value {representation of an empty value} 
+    A null value can be assigned to a variable 
+
+// undefined => when value is not assigned 
+// symbol => to find the uniqueness of an individual component
+
+*/
+
+
+/* OBJECT */
+
+console.log(typeof "shriya")
+
+console.log(typeof null) // type - object
+
+console.log(typeof undefined) // type- undefined
